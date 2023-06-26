9f8d2d9a4bb078bdf0ee6bea6479fc4cc4a0f0ff
FZaKK <2014074@mail.nankai.edu.cn>
2023-06-26 19:52:31 +0800

    add the n param

--- source code/filedeal.h
+++ source code/filedeal.h
@@ -1,21 +1,47 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <map>
 #include "sudoku.h"
 using namespace std;
 
+
+// 难度等级与空格数目的键值对
+map<int, int> diff_map = {
+        {1, 20},
+        {2, 40},
+        {3, 55}
+};
+
+
 // 对应c参数，生成sudo终局文件
-void create_sudo(int num) {
+void create_sudo(int num){
     // 先将曾经生成的内容清空
     ofstream f("board.txt", ios_base::out);
 
-    cout << "Begin to create the sudo board ..." << endl << endl;
-    for (int i = 0; i < num; i++) {
+    cout << "Begin to create the sudo board ..." << endl;
+    for (int i = 0; i < num; i++){
         vector<vector<char>>blank(9, vector<char>(9, '$'));
         sudoku temp(blank);
         temp.create(0, blank);
         temp.outputBoard(blank, f);
     }
     cout << "Finish to create the sudo board !!!" << endl;
+    f.close();
+}
+
+// 对应n参数，生成game数独游戏
+void create_game(int num, int m = 1){
+    // 先将曾经生成的内容清空
+    ofstream f("game.txt", ios_base::out);
+
+    cout << "Begin to create the game board ..." << endl;
+    for (int i = 0; i < num; i++){
+        vector<vector<char>>blank(9, vector<char>(9, '$'));
+        sudoku temp(blank);
+        temp.create(diff_map[m], blank);
+        temp.outputBoard(blank, f);
+    }
+    cout << "Finish to create the game board !!!" << endl;
     f.close();
 }
--- source code/sudoku.cpp
+++ source code/sudoku.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]){
 
     // 补充对应的参数
     a.add<int>("create", 'c', "The number of Sudoku finals required", false, 0, cmdline::range(1, 1000000));
-    a.add<string>("solve", 's', "The path to the Sudoku board file that needs to be solved", false, "game.txt");
+    a.add<string>("solve", 's', "The path to the Sudoku board file that needs to be solved", false, "");
     a.add<int>("number", 'n', "The number of Sudoku games needed", false, 0, cmdline::range(1, 10000));
     a.add<int>("manage", 'm', "The difficulty of generating a Sudoku game", false, 0, cmdline::range(1, 3));
     a.add<int>("range", 'r', "The range of the number of knockouts in the game", false, 0, cmdline::range(20, 55));
@@ -20,11 +20,23 @@ int main(int argc, char* argv[]){
 
     a.parse_check(argc, argv);
 
-    cout << a.get<int>("create") << endl;
-    if (a.exist("unique")) cout << "unique" << endl;
+    // 判断输入的命令，开始测试
+    if (a.get<int>("create") != 0){
+        // 测试生成数独终盘到文件之中
+        create_sudo(a.get<int>("create"));
+    }
+    if (a.get<string>("solve") != "") {
+        cout << "solve the sudoku" << endl;
+    }
+    if (a.get<int>("number") != 0) {
+        create_game(a.get<int>("number"));
+    }
+    
 
-
-    vector<vector<char>>board = { vector<char>{'5','3','$','$','7','$','$','$','$'},
+    // 没有参数的时候会给出数独的example
+    if (argc == 1) {
+        cout << " ----- Show the sudoku example ----- " << endl;
+        vector<vector<char>>board = { vector<char>{'5','3','$','$','7','$','$','$','$'},
                                   vector<char>{'6','$','$','1','9','5','$','$','$'},
                                   vector<char>{'$','9','8','$','$','$','$','6','$'},
                                   vector<char>{'8','$','$','$','6','$','$','$','3'},
@@ -33,33 +45,32 @@ int main(int argc, char* argv[]){
                                   vector<char>{'$','6','$','$','$','$','2','8','$'},
                                   vector<char>{'$','$','$','4','1','9','$','$','5'},
                                   vector<char>{'$','$','$','$','8','$','$','7','9'}, };
-    sudoku temp(board);
-    temp.dfs(board, 0);
-    temp.printBoard(board);
+        sudoku temp(board);
+        temp.dfs(board, 0);
+        temp.printBoard(board);
 
-    // 创建一个9*9的空二维数组
-    vector<vector<char>>blank(9, vector<char>(9, '$'));
-    sudoku s(blank);
-    s.create(50, blank);  // 创建50个空格的数独
-    if (s.active){
-        cout << "创建的数独为：" << endl;
-        s.printBoard(blank);
-    }
-    else {
-        cout << "数独表不合法，没有解！" << endl;
-    }
+        // 创建一个9*9的空二维数组
+        vector<vector<char>>blank(9, vector<char>(9, '$'));
+        sudoku s(blank);
+        s.create(50, blank);  // 创建50个空格的数独
+        if (s.active){
+            cout << endl << " ----- The gen of the sudoku ----- " << endl;
+            s.printBoard(blank);
+        }
+        else{
+            cout << endl << " ----- The sudoku is invalid -----" << endl;
+        }
 
-    s.dfs(blank, 0);//解数独函数
-    if (s.active){
-        cout << "此数独的解是：" << endl;
-        s.printBoard(blank);
+        // 解数独函数
+        s.dfs(blank, 0);
+        if (s.active){
+            cout << endl <<  " ----- The solution of the sudoku ----- " << endl;
+            s.printBoard(blank);
+        }
+        else{
+            cout << endl << " ----- The sudoku is invalid -----" << endl;
+        }
     }
-    else {
-        cout << "解题失败！" << endl;
-    }
-
-    // 测试生成数独终盘到文件之中
-    create_sudo(2);
 
     return 0;
 }
